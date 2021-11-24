a92e1437d86e344bc53fdda6062889de7392ac4d
Jlobblet <jlobblet-github@gmx.co.uk>
2021-11-24 03:18:33 +0000

    Rename variable

--- ProgcompMarker/Mark.fs
+++ ProgcompMarker/Mark.fs
@@ -8,27 +8,26 @@ open Data
 
 let private logger = Log.create "Mark"
 
-
 let markHandler i : WebPart =
     context
         (fun _ ->
             match (getAnswers (string i)).Result with
             | Result.Error e -> $"Internal server error: %s{e}" |> UTF8.bytes |> ServerErrors.internal_error 
-            | Ok d ->
+            | Ok answers ->
                 mapJson
                     (fun (req: MarkRequest) ->
                         let score =
-                            Array.fold2 (fun acc e1 e2 -> acc + if e1 = e2 then 1 else 0) 0 req.Data d
+                            Array.fold2 (fun acc e1 e2 -> acc + if e1 = e2 then 1 else 0) 0 req.Data answers
 
                         logger.log
                             LogLevel.Info
                             (Message.eventX
-                                $"Marking results for for user %s{req.User} problem %u{i}: %u{score}/%i{d.Length}")
+                                $"Marking results for for user %s{req.User} problem %u{i}: %u{score}/%i{answers.Length}")
 
                         logger.log LogLevel.Info (Message.eventX $"Solutions: %A{req.Data}")
-                        logger.log LogLevel.Info (Message.eventX $"Answers: %A{d}")
+                        logger.log LogLevel.Info (Message.eventX $"Answers: %A{answers}")
 
                         { Id = i
                           Score = score
-                          MaxScore = d.Length })
+                          MaxScore = answers.Length })
             )
