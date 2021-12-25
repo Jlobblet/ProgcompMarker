eacb55374658ee17469704327965c71fab50384f
Jlobblet <jlobblet-github@gmx.co.uk>
2021-12-25 18:17:18 +0000

    Parse arguments with Argu + first time setup

--- ProgcompCli/Program.fs
+++ ProgcompCli/Program.fs
@@ -5,110 +5,119 @@ open System.Net.Http
 open System.Text
 open System.Text.Json
 open Common
+open ProgcompCli.Args
 
-let args = Environment.GetCommandLineArgs()
+[<EntryPoint>]
+let main argv =
+    if not <| File.Exists ConfigFile then
+        Settings.firstTimeSetup ()
 
-if args.Length <> 3 then
-    eprintfn $"Usage: %s{args.[0]} ID EXECUTABLE"
-    exit 1
+    let args = Settings.fromArgv argv
+    exit 0
 
-let problemId =
-    match UInt64.TryParse args.[1] with
-    | true, u -> u
-    | false, _ ->
-        eprintfn $"Failed to parse %s{args.[1]} as an unsigned integer."
+    if argv.Length <> 3 then
+        eprintfn $"Usage: %s{argv.[0]} ID EXECUTABLE"
         exit 1
 
-let executable = args.[2]
+    let problemId =
+        match UInt64.TryParse argv.[1] with
+        | true, u -> u
+        | false, _ ->
+            eprintfn $"Failed to parse %s{argv.[1]} as an unsigned integer."
+            exit 1
 
-if not <| File.Exists executable then
-    eprintfn $"Could not find %s{executable} to run."
-    exit 1
+    let executable = argv.[2]
 
-let user =
-    Environment.GetEnvironmentVariable("PROGCOMP_USERNAME", EnvironmentVariableTarget.Process)
+    if not <| File.Exists executable then
+        eprintfn $"Could not find %s{executable} to run."
+        exit 1
 
-if String.IsNullOrWhiteSpace user then
-    eprintfn
-        "Please set your username in the PROGCOMP_USERNAME environment variable (any string will do - it's used to differentiate you from other participants)."
+    let user =
+        Environment.GetEnvironmentVariable("PROGCOMP_USERNAME", EnvironmentVariableTarget.Process)
 
-    exit 1
+    if String.IsNullOrWhiteSpace user then
+        eprintfn
+            "Please set your username in the PROGCOMP_USERNAME environment variable (any string will do - it's used to differentiate you from other participants)."
 
-let endpoint =
+        exit 1
+
+    let endpoint =
 #if DEBUG
-    UriBuilder "http://127.0.0.1:8080/"
+        UriBuilder "http://127.0.0.1:8080/"
 #else
-    let e =
-        Environment.GetEnvironmentVariable("PROGCOMP_ENDPOINT", EnvironmentVariableTarget.Process)
+        let e =
+            Environment.GetEnvironmentVariable("PROGCOMP_ENDPOINT", EnvironmentVariableTarget.Process)
 
-    if String.IsNullOrWhiteSpace e then
-        eprintfn "Please set the PROGCOMP_ENDPOINT environment variable."
-        exit 1
+        if String.IsNullOrWhiteSpace e then
+            eprintfn "Please set the PROGCOMP_ENDPOINT environment variable."
+            exit 1
 
-    UriBuilder e
+        UriBuilder e
 #endif
 
-let httpClient = new HttpClient()
+    let httpClient = new HttpClient()
 
-let inputsResponse =
-    endpoint.Path <- $"/inputs/%u{problemId}"
-    httpClient.GetAsync(endpoint.Uri).Result
+    let inputsResponse =
+        endpoint.Path <- $"/inputs/%u{problemId}"
+        httpClient.GetAsync(endpoint.Uri).Result
 
-if not inputsResponse.IsSuccessStatusCode then
-    eprintfn $"Error reading data from server: %s{inputsResponse.ToString()}"
-    exit 1
+    if not inputsResponse.IsSuccessStatusCode then
+        eprintfn $"Error reading data from server: %s{inputsResponse.ToString()}"
+        exit 1
 
-let inputs =
-    JsonSerializer.Deserialize<InputResponse>(inputsResponse.Content.ReadAsStream())
+    let inputs =
+        JsonSerializer.Deserialize<InputResponse>(inputsResponse.Content.ReadAsStream())
 
-let data = inputs.Data
+    let data = inputs.Data
 
-printfn "Retrieved input data"
+    printfn "Retrieved input data"
 
-let info = ProcessStartInfo()
-info.FileName <- executable
-info.RedirectStandardInput <- true
-info.RedirectStandardOutput <- true
+    let info = ProcessStartInfo()
+    info.FileName <- executable
+    info.RedirectStandardInput <- true
+    info.RedirectStandardOutput <- true
 
-printfn "Running solution..."
+    printfn "Running solution..."
 
-let proc = Process.Start info
-data |> Array.iter proc.StandardInput.WriteLine
-proc.StandardInput.Close()
-proc.WaitForExit()
+    let proc = Process.Start info
+    data |> Array.iter proc.StandardInput.WriteLine
+    proc.StandardInput.Close()
+    proc.WaitForExit()
 
-if proc.ExitCode <> 0 then
-    eprintfn $"Process exited with exit code %i{proc.ExitCode}"
-    exit 1
+    if proc.ExitCode <> 0 then
+        eprintfn $"Process exited with exit code %i{proc.ExitCode}"
+        exit 1
 
-printfn "Complete!"
+    printfn "Complete!"
 
-let answers =
-    proc
-        .StandardOutput
-        .ReadToEnd()
-        .Split(Environment.NewLine)
-    |> Array.filter (not << String.IsNullOrWhiteSpace)
+    let answers =
+        proc
+            .StandardOutput
+            .ReadToEnd()
+            .Split(Environment.NewLine)
+        |> Array.filter (not << String.IsNullOrWhiteSpace)
 
-let req =
-    { Id = inputs.Id
-      User = user
-      Data = answers }
+    let req =
+        { Id = inputs.Id
+          User = user
+          Data = answers }
 
-let content =
-    new StringContent(JsonSerializer.Serialize req, Encoding.UTF8, "application/json")
+    let content =
+        new StringContent(JsonSerializer.Serialize req, Encoding.UTF8, "application/json")
 
-printfn "Sending solutions to server..."
+    printfn "Sending solutions to server..."
 
-let markResponse =
-    endpoint.Path <- $"/mark/%u{inputs.Id}"
-    httpClient.PostAsync(endpoint.Uri, content).Result
+    let markResponse =
+        endpoint.Path <- $"/mark/%u{inputs.Id}"
+        httpClient.PostAsync(endpoint.Uri, content).Result
 
-if not markResponse.IsSuccessStatusCode then
-    eprintfn $"Error reading data from server: %s{markResponse.ToString()}"
-    exit 1
+    if not markResponse.IsSuccessStatusCode then
+        eprintfn $"Error reading data from server: %s{markResponse.ToString()}"
+        exit 1
 
-let score =
-    JsonSerializer.Deserialize<MarkResponse>(markResponse.Content.ReadAsStream())
+    let score =
+        JsonSerializer.Deserialize<MarkResponse>(markResponse.Content.ReadAsStream())
 
-printfn $"Score: %i{score.Score}/%i{score.MaxScore}"
+    printfn $"Score: %i{score.Score}/%i{score.MaxScore}"
+
+    0
---
+++ ProgcompCli/Settings.fs
@@ -0,0 +1,150 @@
+module ProgcompCli.Args
+
+open System
+open System.IO
+open Argu
+
+let ConfigFile = "app.config"
+
+type Arguments =
+    | [<Unique; AltCommandLine("-w")>] Auto_Submit
+    | [<Unique; AltCommandLine("-a")>] Pass_Input_As_Args
+    | [<Mandatory; NoCommandLine>] Endpoint of string
+    | [<Mandatory; NoCommandLine>] Username of string
+    | Executable_Args of arg: string list
+    | [<MainCommand; Mandatory>] Problem_And_Executable of number: uint64 * path: string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Auto_Submit -> "Automatically run the provided executable again when the file is updated."
+            | Pass_Input_As_Args -> "If set, pass the problem input as arguments rather than to standard input."
+            | Endpoint _ -> "URL to the server that will send problem inputs and mark answers."
+            | Username _ -> "A username that is used to differentiate submissions."
+            | Executable_Args _ -> "Arguments to be passed to the executable."
+            | Problem_And_Executable _ ->
+                "Problem number to run and path to the executable to run problem inputs again."
+
+let errorHandler =
+    ProcessExiter(
+        colorizer =
+            function
+            | ErrorCode.HelpText -> None
+            | _ -> Some ConsoleColor.Red
+    )
+
+let private Parser =
+    ArgumentParser<Arguments>(errorHandler = errorHandler, usageStringCharacterWidth = Console.WindowWidth)
+
+type SubmissionMode =
+    | RunOnce
+    | FileWatcherRepeat
+    static member PostProcess result =
+        if result then
+            FileWatcherRepeat
+        else
+            RunOnce
+
+type InputMode =
+    | InputToStdIn
+    | InputAsArgs
+    static member PostProcess result =
+        if result then
+            InputAsArgs
+        else
+            InputToStdIn
+
+type Settings =
+    { SubmissionMode: SubmissionMode
+      PassInputAsArgs: InputMode
+      Endpoint: string
+      Username: string
+      ExecutableArgs: string []
+      ExecutablePath: string
+      ProblemNumber: uint64 }
+
+[<RequireQualifiedAccess>]
+module Settings =
+    let firstTimeSetup () =
+        if Console.IsInputRedirected then
+            eprintfn "Error: cannot run first-time setup if input is redirected."
+            exit 1
+
+        printfn "No configuration file found. First time setup:"
+        let mutable retry = true
+        let mutable endpoint = ""
+
+        while retry do
+            printf "Please enter the endpoint for the server: "
+            endpoint <- Console.ReadLine()
+            printf $"Is %s{endpoint} correct? (Y/n)"
+            let key = Console.ReadKey()
+            printf "\n"
+
+            if [| ConsoleKey.Enter; ConsoleKey.Y |]
+               |> Array.contains key.Key then
+                retry <- false
+
+        // Shadow to enforce immutability now
+        let endpoint = endpoint
+
+        let mutable username = ""
+        retry <- true
+
+        while retry do
+            printf "Please enter a username: "
+            username <- Console.ReadLine()
+            printf $"Is %s{username} correct? (Y/n)"
+            let key = Console.ReadKey()
+            printf "\n"
+
+            if [| ConsoleKey.Enter; ConsoleKey.Y |]
+               |> Array.contains key.Key then
+                retry <- false
+
+        let username = username
+
+        let args = [ Endpoint endpoint; Username username ]
+
+        let config =
+            Parser.PrintAppSettingsArguments(args, true)
+
+        File.WriteAllText(ConfigFile, config, Text.Encoding.UTF8)
+
+        printf "Submit solution? (y/N)"
+        let key = Console.ReadKey()
+        printf "\n"
+
+        if key.Key <> ConsoleKey.Y then exit 0
+
+    let fromArgv argv =
+        let configurationReader =
+            ConfigurationReader.FromAppSettingsFile ConfigFile
+
+        let results = Parser.Parse(argv, configurationReader)
+
+        let submissionMode =
+            results.Contains <@ Auto_Submit @>
+            |> SubmissionMode.PostProcess
+
+        let passInputAsArgs =
+            results.Contains <@ Pass_Input_As_Args @>
+            |> InputMode.PostProcess
+
+        let endpoint = results.GetResult <@ Endpoint @>
+        let username = results.GetResult <@ Username @>
+
+        let executableArgs =
+            results.TryGetResult <@ Executable_Args @>
+            |> Option.fold (fun _ -> Array.ofList) Array.empty
+
+        let problemNumber, executablePath =
+            results.GetResult <@ Problem_And_Executable @>
+
+        { SubmissionMode = submissionMode
+          PassInputAsArgs = passInputAsArgs
+          Endpoint = endpoint
+          Username = username
+          ExecutableArgs = executableArgs
+          ExecutablePath = executablePath
+          ProblemNumber = problemNumber }
