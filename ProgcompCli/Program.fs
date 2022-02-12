53e8dbbc50fc14e2e4f08636eeb5fa185b2ad3f2
Jlobblet <jlobblet-github@gmx.co.uk>
2022-02-12 14:58:47 +0000

    Extract a bunch of functions

--- ProgcompCli/Program.fs
+++ ProgcompCli/Program.fs
@@ -7,6 +7,84 @@ open System.Text.Json
 open Common
 open ProgcompCli.Args
 
+let getInputs (endpoint: UriBuilder) (httpClient: HttpClient) settings =
+    let inputsResponse =
+        endpoint.Path <- $"/inputs/%u{settings.ProblemNumber}"
+        httpClient.GetAsync(endpoint.Uri).Result
+
+    if not inputsResponse.IsSuccessStatusCode then
+        eprintfn $"Error reading data from server: %O{inputsResponse}"
+        exit 1
+
+    let response =
+        JsonSerializer.Deserialize<InputResponse>(inputsResponse.Content.ReadAsStream())
+
+    printfn "Retrieved input data"
+
+    response
+
+let getProcessStartInfo settings data =
+    let info = ProcessStartInfo()
+    info.FileName <- settings.ExecutablePath
+    info.RedirectStandardInput <- true
+    info.RedirectStandardOutput <- true
+
+    settings.ExecutableArgs
+    |> Array.iter info.ArgumentList.Add
+
+    if settings.InputMode = InputAsArgs then
+        if not <| Array.isEmpty settings.ExecutableArgs then
+            info.ArgumentList.Add "--"
+
+        Array.iter info.ArgumentList.Add data
+
+    info
+
+let runSolution (data: string []) settings (info: ProcessStartInfo) =
+    printfn "Running solution..."
+
+    let proc = Process.Start info
+
+    if settings.InputMode = InputToStdIn then
+        data |> Array.iter proc.StandardInput.WriteLine
+
+        proc.StandardInput.Close()
+
+    proc.WaitForExit()
+
+    if proc.ExitCode <> 0 then
+        eprintfn $"Process exited with nonzero exit code %i{proc.ExitCode}, quitting"
+        exit 1
+
+    printfn "Complete!"
+
+    proc
+        .StandardOutput
+        .ReadToEnd()
+        .Split(Environment.NewLine)
+    |> Array.filter (not << String.IsNullOrWhiteSpace)
+
+let sendResults (endpoint: UriBuilder) (httpClient: HttpClient) (inputs: InputResponse) settings answers =
+    let req =
+        { Id = inputs.Id
+          User = settings.Username
+          Data = answers }
+
+    let content =
+        new StringContent(JsonSerializer.Serialize req, Encoding.UTF8, "application/json")
+
+    printfn "Sending solutions to server..."
+
+    let markResponse =
+        endpoint.Path <- $"/mark/%u{inputs.Id}"
+        httpClient.PostAsync(endpoint.Uri, content).Result
+
+    if not markResponse.IsSuccessStatusCode then
+        eprintfn $"Error reading data from server: %s{markResponse.ToString()}"
+        exit 1
+
+    JsonSerializer.Deserialize<MarkResponse>(markResponse.Content.ReadAsStream())
+
 [<EntryPoint>]
 let main argv =
     if not <| File.Exists ConfigFile then
@@ -27,78 +105,16 @@ let main argv =
         settings.Endpoint
 #endif
 
-    let httpClient = new HttpClient()
+    use httpClient = new HttpClient()
 
-    let inputsResponse =
-        endpoint.Path <- $"/inputs/%u{settings.ProblemNumber}"
-        httpClient.GetAsync(endpoint.Uri).Result
+    let inputs = getInputs endpoint httpClient settings
 
-    if not inputsResponse.IsSuccessStatusCode then
-        eprintfn $"Error reading data from server: %O{inputsResponse}"
-        exit 1
+    let info = getProcessStartInfo settings inputs.Data
 
-    let inputs =
-        JsonSerializer.Deserialize<InputResponse>(inputsResponse.Content.ReadAsStream())
-
-    let data = inputs.Data
-
-    printfn "Retrieved input data"
-
-    let info = ProcessStartInfo()
-    info.FileName <- executable
-    info.RedirectStandardInput <- true
-    info.RedirectStandardOutput <- true
-    settings.ExecutableArgs |> Array.iter info.ArgumentList.Add
-        
-    if settings.InputMode = InputAsArgs then
-        if not <| Array.isEmpty settings.ExecutableArgs then
-            info.ArgumentList.Add "--"
-            
-        data |> Array.iter info.ArgumentList.Add
-
-    printfn "Running solution..."
-
-    let proc = Process.Start info
-    
-    if settings.InputMode = InputToStdIn then
-        data |> Array.iter proc.StandardInput.WriteLine
-        proc.StandardInput.Close()
-
-    proc.WaitForExit()
-
-    if proc.ExitCode <> 0 then
-        eprintfn $"Process exited with exit code %i{proc.ExitCode}"
-        exit 1
-
-    printfn "Complete!"
-
-    let answers =
-        proc
-            .StandardOutput
-            .ReadToEnd()
-            .Split(Environment.NewLine)
-        |> Array.filter (not << String.IsNullOrWhiteSpace)
-
-    let req =
-        { Id = inputs.Id
-          User = settings.Username
-          Data = answers }
-
-    let content =
-        new StringContent(JsonSerializer.Serialize req, Encoding.UTF8, "application/json")
-
-    printfn "Sending solutions to server..."
-
-    let markResponse =
-        endpoint.Path <- $"/mark/%u{inputs.Id}"
-        httpClient.PostAsync(endpoint.Uri, content).Result
-
-    if not markResponse.IsSuccessStatusCode then
-        eprintfn $"Error reading data from server: %s{markResponse.ToString()}"
-        exit 1
+    let answers = runSolution inputs.Data settings info
 
     let score =
-        JsonSerializer.Deserialize<MarkResponse>(markResponse.Content.ReadAsStream())
+        sendResults endpoint httpClient inputs settings answers
 
     printfn $"Score: %i{score.Score}/%i{score.MaxScore}"
 
